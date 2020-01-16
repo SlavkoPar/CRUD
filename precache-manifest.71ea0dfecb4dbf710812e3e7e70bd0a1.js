b5c7677355374fafb1447d4acaad02ccbff4b080
SlavkoPar <slavko.parezanin@gmail.com>
2020-01-16 11:45:07 +0100

    Updates

---
+++ precache-manifest.71ea0dfecb4dbf710812e3e7e70bd0a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105805b7b4e4a533e32b6ec04842d2d3",
+    "url": "/CRUD/index.html"
+  },
+  {
+    "revision": "ec9c69f016191887f63a",
+    "url": "/CRUD/static/css/main.ddbea72d.chunk.css"
+  },
+  {
+    "revision": "23d215d1573ee58422ac",
+    "url": "/CRUD/static/js/2.77673861.chunk.js"
+  },
+  {
+    "revision": "1456219fd5038fc13a9ea1ef10f897c5",
+    "url": "/CRUD/static/js/2.77673861.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec9c69f016191887f63a",
+    "url": "/CRUD/static/js/main.5c6ee04f.chunk.js"
+  },
+  {
+    "revision": "331dcbe2d46673dd64b7",
+    "url": "/CRUD/static/js/runtime-main.b9ddf62d.js"
+  }
+]);
